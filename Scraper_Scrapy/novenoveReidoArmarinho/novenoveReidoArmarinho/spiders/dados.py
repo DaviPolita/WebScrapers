ea60f429e0efad26e95a8a23985adbd0bc0724b7
DaviPolita <davipolita@gmail.com>
2020-08-12 17:13:50 -0300

    Nova extrutura de pastas

--- Scraper_Scrapy/novenoveReidoArmarinho/novenoveReidoArmarinho/spiders/dados.py
+++ Scraper_Scrapy/novenoveReidoArmarinho/novenoveReidoArmarinho/spiders/dados.py
@@ -1,12 +1,12 @@
-import sqlite3
-import pandas as pd
-# Create your connection.
-cnx = sqlite3.connect('rei_do_armarinho.db')
+# import sqlite3
+# import pandas as pd
+# # Create your connection.
+# cnx = sqlite3.connect('rei_do_armarinho.db')
 
-df = pd.read_sql_query("SELECT * FROM produtos_tb", cnx)
+# df = pd.read_sql_query("SELECT * FROM produtos_tb", cnx)
 
-# df.to_csv("preços.csv")
-df.to_excel("produtos.xlsx")
+# # df.to_csv("preços.csv")
+# df.to_excel("produtos.xlsx")
 
 
 # # print(len(test))
